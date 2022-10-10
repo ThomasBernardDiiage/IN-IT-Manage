916c9f3a61e1d2fffff8042a429abdc689c8ec74
Thomas Bernard <thomas.bernard@diiage.org>
2022-10-10 16:32:00 +0200

    refacto

--- InitManage/InitManage/Services/BookingService.cs
+++ InitManage/InitManage/Services/BookingService.cs
@@ -6,44 +6,43 @@ using InitManage.Services.Interfaces;
 using Simple.Http;
 using System.Net;
 
-namespace InitManage.Services
+namespace InitManage.Services;
+
+public class BookingService: IBookingService
 {
-    public class BookingService: IBookingService
+    private readonly IHttpService _httpService;
+
+    public BookingService(IHttpService httpService)
     {
-        private readonly IHttpService _httpService;
+        _httpService = httpService;
+    }
 
-        public BookingService(IHttpService httpService)
+    public Task<IBookingEntity> GetBookingAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<IBookingEntity>> GetBookingsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<BookingWrapper>> GetBookingsWrappersAsync()
+    {
+        var response = await _httpService.SendRequestAsync<IEnumerable<DetailledBookingDTODown>>($"{Constants.ApiBaseAdress}{Constants.BookingEndPoint}/{Constants.UserEndPoint}", HttpMethod.Get);
+
+        if (response.HttpStatusCode == HttpStatusCode.OK)
         {
-            _httpService = httpService;
+            var wrappers = response.Result?.Select(x => new BookingWrapper(x));
+            return wrappers;
         }
+        return null;
+    }
 
-        public Task<IBookingEntity> GetBookingAsync(long id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<IBookingEntity>> GetBookingsAsync()
-        {
-            throw new NotImplementedException();
-        }
-
-        public async Task<IEnumerable<BookingWrapper>> GetBookingsWrappersAsync()
-        {
-            var response = await _httpService.SendRequestAsync<IEnumerable<DetailledBookingDTODown>>($"{Constants.ApiBaseAdress}{Constants.BookingEndPoint}/{Constants.UserEndPoint}", HttpMethod.Get);
-
-            if (response.HttpStatusCode == HttpStatusCode.OK)
-            {
-                var wrappers = response.Result?.Select(x => new BookingWrapper(x));
-                return wrappers;
-            }
-            return null;
-        }
-
-        public async Task<bool> CreateBookingAsync(IBookingEntity booking)
-        {
-            var dto = new BookingDTOUp(booking);
-            var result = await _httpService.SendRequestAsync($"{Constants.ApiBaseAdress}{Constants.BookingEndPoint}", HttpMethod.Post, dto);
-            return result.HttpStatusCode == HttpStatusCode.OK;
-        }
+    public async Task<bool> CreateBookingAsync(IBookingEntity booking)
+    {
+        var dto = new BookingDTOUp(booking);
+        var result = await _httpService.SendRequestAsync($"{Constants.ApiBaseAdress}{Constants.BookingEndPoint}", HttpMethod.Post, dto);
+        return result.HttpStatusCode == HttpStatusCode.OK;
     }
 }
